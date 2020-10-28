88b51c4f89325c7641b40a4a14ae6f79c73f8548
W42656 <W42656@172.60.134.67>
2020-10-28 23:30:22 +0900

    2단

---
+++ src/gugudan.java
@@ -0,0 +1,14 @@
+
+public class gugudan {
+	public static void main(String[] orgs) {
+		int a = 2;
+		int i = 1;
+		while(i < 10) {
+			int result = a * i;
+			i = i + 1;
+			System.out.println(result);
+		}
+		
+	}
+
+}
