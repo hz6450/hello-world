3c38120b908f9e75bec71501ec7a8a2463abd528
W42656 <W42656@172.60.134.67>
2020-10-28 23:37:31 +0900

    구구단

--- src/gugudan.java
+++ src/gugudan.java
@@ -3,12 +3,16 @@ public class gugudan {
 	public static void main(String[] orgs) {
 		int a = 2;
 		int i = 1;
-		while(i < 10) {
-			int result = a * i;
-			i = i + 1;
-			System.out.println(result);
+		while(a < 10) {
+			System.out.println(a+"´Ü Ãâ·Â");
+			while(i < 10) {
+				int result = a * i;
+				i = i + 1;
+				System.out.println(result);
+			}
+			i = 1;
+			a = a+1;
 		}
-		
 	}
 
 }
